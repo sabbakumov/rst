3b0a3992e5c22da2afd0f3261a250c5b6afdcd39
Sergey Abbakumov <sabbakumov@gmail.com>
2015-04-02 20:56:12 +0600

    Removed unnecessary header files

--- rst/Status/Test.cpp
+++ rst/Status/Test.cpp
@@ -25,7 +25,7 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#include "rst/Status.h"
+#include "rst/Status/Status.h"
 
 #include "gtest/gtest.h"
 
